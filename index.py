eb7ee5149672597ce5f44425f2bdac1d8f10a02c
AdelineMartineau <adeline.martineau1@outlook.fr>
2022-10-13 10:02:36 +0200

    adding change

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
-now = datetime.now().strftime("%H:%M:%S")
+now = datetime.now()
 today8am = now.replace(hour=8, minute=0, second=0, microsecond=0)
 today18am = now.replace(hour=18, minute=0, second=0, microsecond=0)
 
 def sayHello() :
-    if today8am < now < today18am:
+    if  now < today18am:
         print("Bonjour")
     else:
         print("Bonsoir")
